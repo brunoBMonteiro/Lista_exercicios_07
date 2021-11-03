37d9a105a7c3c1d211d630a716d8eb281dc5d682
Bruno <brunobilheri@hotmail.com>
2021-11-03 10:55:29 -0300

    passagem parametro ou argumento

--- ExemploMetodosComParametros/src/TesteCarro.java
+++ ExemploMetodosComParametros/src/TesteCarro.java
@@ -1,5 +1,27 @@
 public class TesteCarro {
     public static void main(String[] args){
 
+        Carro van = new Carro();
+        van.marca = "Fiat";
+        van.modelo = "Ducato";
+        van.numPassageiros = 10;
+        van.capCombustivel = 100;
+        van.consumoCombustivel = 0.2;
+
+        System.out.println(van.marca);
+        System.out.println(van.modelo);
+
+        van.exibirAutonomia();
+
+        double autonomia = van.obterAutonomia();
+        System.out.println("A autonomia do carro é: " + autonomia);
+        System.out.println("A autonomia do carro é : " + van.obterAutonomia());
+
+
+        double qtdCombustivel50 = van.calcularCombustivel(50);
+        double qtdCombustivel60 = van.calcularCombustivel(60);
+
+        System.out.println("qtdCombustivel50 = " + qtdCombustivel50);
+        System.out.println("qtdCombustivel60 = " + qtdCombustivel60);
     }
 }
